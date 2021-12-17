8d503bbdbbba2d633d77771a57ef4db77035ba63
Miquel Bontes <JoellB@hotmail.nl>
2021-12-17 21:45:48 +0100

    first release

---
+++ _nuxt/52f58f2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{170:function(n,t,e){"use strict";var c={},r=e(40),component=Object(r.a)(c,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",{staticClass:"min-h-screen bg-gray-800"},[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},172:function(n,t,e){e(173),n.exports=e(174)}},[[172,8,1,9]]]);
